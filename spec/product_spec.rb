255662a7939bc456b9ec830d64aef8fe52aae849
Bosi Chen <chenbosi.bosi@gmail.com>
2022-01-11 19:51:48 -0500

    rewrite test for product model

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -4,76 +4,56 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
 
-    describe "product" do
-      it 'should have the 4 fields validated' do
-        @category1 = Category.find_or_create_by! name: 'category1'
-        @product = @category1.products.create!({
-          name:  'Test Product',
-          quantity: 10,
-          price: 10.00
-        })
-        expect(@product.name).not_to be nil
-        expect(@product.price).not_to be nil
-        expect(@product.quantity).not_to be nil
-        expect(@product.category).not_to be nil
-      end
+    it "is valid with valid attributes" do
+      @category1 = Category.find_or_create_by! name: 'category1'
+      @product = @category1.products.create!({
+        name:  'Test Product',
+        quantity: 10,
+        price: 10.00
+      })
+      expect(@product).to be_valid
     end
 
-    # validate name
-    describe "product" do
-      before do
-        @category1 = Category.find_or_create_by! name: 'category1'
-        @product = @category1.products.create!({
-          name: nil,
-          quantity: 4,
-          price: 34.99
-        })
-      end
-      it 'expect product name to be validate' do
-        expect(@product.name).to be nil
-      end
+    it "is not valid without a name" do
+      @category1 = Category.find_or_create_by! name: 'category1'
+      @product = Product.new({
+        name: nil,
+        quantity: 4,
+        price: 34.99,
+        category: @category1
+      })
+      expect(@product).to_not be_valid
     end
 
-    describe "product" do
-      before do
-        @category1 = Category.find_or_create_by! name: 'category1'
-        @product = @category1.products.create!({
-          name:  'Test Product 2',
-          quantity: 10,
-          price: nil
-        })
-      end
-      it 'expect product price to be validate' do
-        expect(@product.price).to be nil
-      end
+    it "is not valid without a price" do
+      @category1 = Category.find_or_create_by! name: 'category1'
+      @product = Product.new({
+        name:  'Test Product 2',
+        quantity: 10,
+        price: nil
+      })
+      expect(@product).to_not be_valid
     end
 
-    describe "product" do
-      before do
-        @category1 = Category.find_or_create_by! name: 'category1'
-        @product = @category1.products.create!({
-          name:  'Test Product 3',
-          quantity: nil,
-          price: 24.99
-        })
-      end
-      it 'expect product quantity to be validate' do
-        expect(@product.quantity).to be nil
-      end
+    it "is not valid without a quantity" do
+      @category1 = Category.find_or_create_by! name: 'category1'
+      @product = Product.new({
+        name: 'Test Product 3',
+        quantity: nil,
+        price: 19.99,
+        category: @category1
+      })
+      expect(@product).to_not be_valid
     end
 
-    describe "product" do
-      before do
-        @category2 = nil
-        @product = @category2.products.create!({
-          name:  'Test Product 4',
-          quantity: 29,
-          price: 4.99
-        })
-      end
-      it 'expect product category to be validate' do
-        expect(@product.category).to be nil
-      end
+    it "is not valid without a category" do
+      @product = Product.new({
+        name: 'Test Product 4',
+        quantity: 29,
+        price: 4.99,
+        category: nil
+      })
+      expect(@product).to_not be_valid
     end
 
   end
